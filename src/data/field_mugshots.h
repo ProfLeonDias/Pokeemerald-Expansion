007ffb1bd98039048cb1a3ff75977b1fb673013a
EmperorZaph <104250397+EmperorZaph@users.noreply.github.com>
2026-01-23 11:53:43 -0500

    Uhh

---
+++ src/data/field_mugshots.h
@@ -0,0 +1,433 @@
+
+static const u32 sFieldMugshotGfx_Jirachi [] = INCBIN_U32("graphics/field_mugshots/test/1_Jirachi.4bpp.lz");
+static const u32 sFieldMugshotGfx_JirachiShadow [] = INCBIN_U32("graphics/field_mugshots/test/2_Jirachi_Shadow.4bpp.lz");
+static const u32 sFieldMugshotGfx_Cresselia [] = INCBIN_U32("graphics/field_mugshots/test/3_Cresselia.4bpp.lz");
+static const u32 sFieldMugshotGfx_Darkrai [] = INCBIN_U32("graphics/field_mugshots/test/4_Darkrai.4bpp.lz");
+static const u32 sFieldMugshotGfx_Psychic [] = INCBIN_U32("graphics/field_mugshots/test/5_Psychic_Girl.4bpp.lz");
+static const u32 sFieldMugshotGfx_Anthony [] = INCBIN_U32("graphics/field_mugshots/test/6_Anthony.4bpp.lz");
+static const u32 sFieldMugshotGfx_Egypt [] = INCBIN_U32("graphics/field_mugshots/test/7_Egypt.4bpp.lz");
+static const u32 sFieldMugshotGfx_Mom [] = INCBIN_U32("graphics/field_mugshots/test/8_Mom.4bpp.lz");
+static const u32 sFieldMugshotGfx_ProfAspen [] = INCBIN_U32("graphics/field_mugshots/test/9_Prof_Aspen.4bpp.lz");
+static const u32 sFieldMugshotGfx_Alex [] = INCBIN_U32("graphics/field_mugshots/test/10_Alex.4bpp.lz");
+static const u32 sFieldMugshotGfx_Katelyn [] = INCBIN_U32("graphics/field_mugshots/test/11_Katelyn.4bpp.lz");
+static const u32 sFieldMugshotGfx_Lyon [] = INCBIN_U32("graphics/field_mugshots/test/12_Lyon.4bpp.lz");
+static const u32 sFieldMugshotGfx_FiaRobed [] = INCBIN_U32("graphics/field_mugshots/test/13_Fiasendra_Robed.4bpp.lz");
+static const u32 sFieldMugshotGfx_Fia [] = INCBIN_U32("graphics/field_mugshots/test/14_Fiasendra_Unrobed.4bpp.lz");
+static const u32 sFieldMugshotGfx_FinnRobed [] = INCBIN_U32("graphics/field_mugshots/test/15_Finn_Robed.4bpp.lz");
+static const u32 sFieldMugshotGfx_Finn [] = INCBIN_U32("graphics/field_mugshots/test/16_Finn_Unrobed.4bpp.lz");
+static const u32 sFieldMugshotGfx_MissosRobed [] = INCBIN_U32("graphics/field_mugshots/test/17_Missos_Robed.4bpp.lz");
+static const u32 sFieldMugshotGfx_Missos [] = INCBIN_U32("graphics/field_mugshots/test/18_Missos_Unrobed.4bpp.lz");
+static const u32 sFieldMugshotGfx_SophiaRobed [] = INCBIN_U32("graphics/field_mugshots/test/19_Sophia_Robed.4bpp.lz");
+static const u32 sFieldMugshotGfx_Sophia [] = INCBIN_U32("graphics/field_mugshots/test/20_Sophia_Unrobed.4bpp.lz");
+static const u32 sFieldMugshotGfx_Zaph [] = INCBIN_U32("graphics/field_mugshots/test/21_Zaph.4bpp.lz");
+static const u32 sFieldMugshotGfx_Petra [] = INCBIN_U32("graphics/field_mugshots/test/22_Petra.4bpp.lz");
+static const u32 sFieldMugshotGfx_Sid [] = INCBIN_U32("graphics/field_mugshots/test/23_Sid.4bpp.lz");
+static const u32 sFieldMugshotGfx_Derryl [] = INCBIN_U32("graphics/field_mugshots/test/24_Derryl.4bpp.lz");
+static const u32 sFieldMugshotGfx_Stelio [] = INCBIN_U32("graphics/field_mugshots/test/25_Stelio.4bpp.lz");
+static const u32 sFieldMugshotGfx_Adeline [] = INCBIN_U32("graphics/field_mugshots/test/26_Adeline.4bpp.lz");
+static const u32 sFieldMugshotGfx_Laira [] = INCBIN_U32("graphics/field_mugshots/test/27_Laira.4bpp.lz");
+static const u32 sFieldMugshotGfx_Autumn [] = INCBIN_U32("graphics/field_mugshots/test/28_Autumn.4bpp.lz");
+static const u32 sFieldMugshotGfx_Raba [] = INCBIN_U32("graphics/field_mugshots/test/29_Raba.4bpp.lz");
+static const u32 sFieldMugshotGfx_Altair [] = INCBIN_U32("graphics/field_mugshots/test/30_Altair.4bpp.lz");
+static const u32 sFieldMugshotGfx_Zachary [] = INCBIN_U32("graphics/field_mugshots/test/31_Zachary.4bpp.lz");
+static const u32 sFieldMugshotGfx_Jade [] = INCBIN_U32("graphics/field_mugshots/test/32_Jade.4bpp.lz");
+static const u32 sFieldMugshotGfx_Red [] = INCBIN_U32("graphics/field_mugshots/test/33_Red.4bpp.lz");
+static const u32 sFieldMugshotGfx_Blue [] = INCBIN_U32("graphics/field_mugshots/test/34_Blue.4bpp.lz");
+static const u32 sFieldMugshotGfx_Lance [] = INCBIN_U32("graphics/field_mugshots/test/35_Lance.4bpp.lz");
+static const u32 sFieldMugshotGfx_Steven [] = INCBIN_U32("graphics/field_mugshots/test/36_Steven.4bpp.lz");
+static const u32 sFieldMugshotGfx_Cynthia [] = INCBIN_U32("graphics/field_mugshots/test/37_Cynthia.4bpp.lz");
+//static const u32 sFieldMugshotGfx_Iris [] = INCBIN_U32("graphics/field_mugshots/test/38_Iris.4bpp.lz");
+//static const u32 sFieldMugshotGfx_Colress [] = INCBIN_U32("graphics/field_mugshots/test/39_Colress.4bpp.lz");
+static const u32 sFieldMugshotGfx_Diantha [] = INCBIN_U32("graphics/field_mugshots/test/40_Diantha.4bpp.lz");
+static const u32 sFieldMugshotGfx_Leon [] = INCBIN_U32("graphics/field_mugshots/test/41_Leon.4bpp.lz");
+static const u32 sFieldMugshotGfx_Nemona [] = INCBIN_U32("graphics/field_mugshots/test/42_Nemona.4bpp.lz");
+
+static const u16 sFieldMugshotPal_Jirachi [] = INCBIN_U16("graphics/field_mugshots/test/1_Jirachi.gbapal");
+static const u16 sFieldMugshotPal_JirachiShadow [] = INCBIN_U16("graphics/field_mugshots/test/2_Jirachi_Shadow.gbapal");
+static const u16 sFieldMugshotPal_Cresselia [] = INCBIN_U16("graphics/field_mugshots/test/3_Cresselia.gbapal");
+static const u16 sFieldMugshotPal_Darkrai [] = INCBIN_U16("graphics/field_mugshots/test/4_Darkrai.gbapal");
+static const u16 sFieldMugshotPal_Psychic [] = INCBIN_U16("graphics/field_mugshots/test/5_Psychic_Girl.gbapal");
+static const u16 sFieldMugshotPal_Anthony [] = INCBIN_U16("graphics/field_mugshots/test/6_Anthony.gbapal");
+static const u16 sFieldMugshotPal_Egypt [] = INCBIN_U16("graphics/field_mugshots/test/7_Egypt.gbapal");
+static const u16 sFieldMugshotPal_Mom [] = INCBIN_U16("graphics/field_mugshots/test/8_Mom.gbapal");
+static const u16 sFieldMugshotPal_ProfAspen [] = INCBIN_U16("graphics/field_mugshots/test/9_Prof_Aspen.gbapal");
+static const u16 sFieldMugshotPal_Alex [] = INCBIN_U16("graphics/field_mugshots/test/10_Alex.gbapal");
+static const u16 sFieldMugshotPal_Katelyn [] = INCBIN_U16("graphics/field_mugshots/test/11_Katelyn.gbapal");
+static const u16 sFieldMugshotPal_Lyon [] = INCBIN_U16("graphics/field_mugshots/test/12_Lyon.gbapal");
+static const u16 sFieldMugshotPal_FiaRobed [] = INCBIN_U16("graphics/field_mugshots/test/13_Fiasendra_Robed.gbapal");
+static const u16 sFieldMugshotPal_Fia [] = INCBIN_U16("graphics/field_mugshots/test/14_Fiasendra_Unrobed.gbapal");
+static const u16 sFieldMugshotPal_FinnRobed [] = INCBIN_U16("graphics/field_mugshots/test/15_Finn_Robed.gbapal");
+static const u16 sFieldMugshotPal_Finn [] = INCBIN_U16("graphics/field_mugshots/test/16_Finn_Unrobed.gbapal");
+static const u16 sFieldMugshotPal_MissosRobed [] = INCBIN_U16("graphics/field_mugshots/test/17_Missos_Robed.gbapal");
+static const u16 sFieldMugshotPal_Missos [] = INCBIN_U16("graphics/field_mugshots/test/18_Missos_Unrobed.gbapal");
+static const u16 sFieldMugshotPal_SophiaRobed [] = INCBIN_U16("graphics/field_mugshots/test/19_Sophia_Robed.gbapal");
+static const u16 sFieldMugshotPal_Sophia [] = INCBIN_U16("graphics/field_mugshots/test/20_Sophia_Unrobed.gbapal");
+static const u16 sFieldMugshotPal_Zaph [] = INCBIN_U16("graphics/field_mugshots/test/21_Zaph.gbapal");
+static const u16 sFieldMugshotPal_Petra [] = INCBIN_U16("graphics/field_mugshots/test/22_Petra.gbapal");
+static const u16 sFieldMugshotPal_Sid [] = INCBIN_U16("graphics/field_mugshots/test/23_Sid.gbapal");
+static const u16 sFieldMugshotPal_Derryl [] = INCBIN_U16("graphics/field_mugshots/test/24_Derryl.gbapal");
+static const u16 sFieldMugshotPal_Stelio [] = INCBIN_U16("graphics/field_mugshots/test/25_Stelio.gbapal");
+static const u16 sFieldMugshotPal_Adeline [] = INCBIN_U16("graphics/field_mugshots/test/26_Adeline.gbapal");
+static const u16 sFieldMugshotPal_Laira [] = INCBIN_U16("graphics/field_mugshots/test/27_Laira.gbapal");
+static const u16 sFieldMugshotPal_Autumn [] = INCBIN_U16("graphics/field_mugshots/test/28_Autumn.gbapal");
+static const u16 sFieldMugshotPal_Raba [] = INCBIN_U16("graphics/field_mugshots/test/29_Raba.gbapal");
+static const u16 sFieldMugshotPal_Altair [] = INCBIN_U16("graphics/field_mugshots/test/30_Altair.gbapal");
+static const u16 sFieldMugshotPal_Zachary [] = INCBIN_U16("graphics/field_mugshots/test/31_Zachary.gbapal");
+static const u16 sFieldMugshotPal_Jade [] = INCBIN_U16("graphics/field_mugshots/test/32_Jade.gbapal");
+static const u16 sFieldMugshotPal_Red [] = INCBIN_U16("graphics/field_mugshots/test/33_Red.gbapal");
+static const u16 sFieldMugshotPal_Blue [] = INCBIN_U16("graphics/field_mugshots/test/34_Blue.gbapal");
+static const u16 sFieldMugshotPal_Lance [] = INCBIN_U16("graphics/field_mugshots/test/35_Lance.gbapal");
+static const u16 sFieldMugshotPal_Steven [] = INCBIN_U16("graphics/field_mugshots/test/36_Steven.gbapal");
+static const u16 sFieldMugshotPal_Cynthia [] = INCBIN_U16("graphics/field_mugshots/test/37_Cynthia.gbapal");
+//static const u16 sFieldMugshotPal_Iris [] = INCBIN_U16("graphics/field_mugshots/test/38_Iris.gbapal");
+//static const u16 sFieldMugshotPal_Colress [] = INCBIN_U16("graphics/field_mugshots/test/39_Colress.gbapal");
+static const u16 sFieldMugshotPal_Diantha [] = INCBIN_U16("graphics/field_mugshots/test/40_Diantha.gbapal");
+static const u16 sFieldMugshotPal_Leon [] = INCBIN_U16("graphics/field_mugshots/test/41_Leon.gbapal");
+static const u16 sFieldMugshotPal_Nemona [] = INCBIN_U16("graphics/field_mugshots/test/42_Nemona.gbapal");
+
+struct MugshotGfx
+{
+    const u32 *gfx;
+    const u16 *pal;
+};
+
+static const struct MugshotGfx sFieldMugshots[MUGSHOT_COUNT][EMOTE_COUNT] =
+{
+    [MUGSHOT_JIRACHI] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Jirachi,
+            .pal = sFieldMugshotPal_Jirachi,
+        },
+    },
+        [MUGSHOT_JIRACHI_2] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_JirachiShadow,
+            .pal = sFieldMugshotPal_JirachiShadow,
+        },
+    },
+        [MUGSHOT_CRESSELIA] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Cresselia,
+            .pal = sFieldMugshotPal_Cresselia,
+        },
+    },
+        [MUGSHOT_DARKRAI] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Darkrai,
+            .pal = sFieldMugshotPal_Darkrai,
+        },
+    },
+        [MUGSHOT_PSYCHIC] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Psychic,
+            .pal = sFieldMugshotPal_Psychic,
+        },
+    },
+        [MUGSHOT_ANTHONY] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Anthony,
+            .pal = sFieldMugshotPal_Anthony,
+        },
+    },
+    [MUGSHOT_EGYPT] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Egypt,
+            .pal = sFieldMugshotPal_Egypt,
+        },
+    },
+        [MUGSHOT_MOM] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Mom,
+            .pal = sFieldMugshotPal_Mom,
+        },
+    },
+        [MUGSHOT_PROF_ASPEN] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_ProfAspen,
+            .pal = sFieldMugshotPal_ProfAspen,
+        },
+    },
+        [MUGSHOT_ALEX] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Alex,
+            .pal = sFieldMugshotPal_Alex,
+        },
+    },
+        [MUGSHOT_KATELYN] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Katelyn,
+            .pal = sFieldMugshotPal_Katelyn,
+        },
+    },
+        [MUGSHOT_LYON] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Lyon,
+            .pal = sFieldMugshotPal_Lyon,
+        },
+    },
+        [MUGSHOT_FIA_ROBED] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_FiaRobed,
+            .pal = sFieldMugshotPal_FiaRobed,
+        },
+    },
+        [MUGSHOT_FIA] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Fia,
+            .pal = sFieldMugshotPal_Fia,
+        },
+    },
+        [MUGSHOT_FINN_ROBED] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_FinnRobed,
+            .pal = sFieldMugshotPal_FinnRobed,
+        },
+    },
+        [MUGSHOT_FINN] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Finn,
+            .pal = sFieldMugshotPal_Finn,
+        },
+    },
+        [MUGSHOT_MISSOS_ROBED] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_MissosRobed,
+            .pal = sFieldMugshotPal_MissosRobed,
+        },
+    },
+        [MUGSHOT_MISSOS] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Missos,
+            .pal = sFieldMugshotPal_Missos,
+        },
+    },
+        [MUGSHOT_SOPHIA_ROBED] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_SophiaRobed,
+            .pal = sFieldMugshotPal_SophiaRobed,
+        },
+    },
+        [MUGSHOT_SOPHIA] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Sophia,
+            .pal = sFieldMugshotPal_Sophia,
+        },
+    },
+        [MUGSHOT_ZAPH] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Zaph,
+            .pal = sFieldMugshotPal_Zaph,
+        },
+    },
+        [MUGSHOT_PETRA] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Petra,
+            .pal = sFieldMugshotPal_Petra,
+        },
+    },
+        [MUGSHOT_SID] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Sid,
+            .pal = sFieldMugshotPal_Sid,
+        },
+    },
+        [MUGSHOT_DERRYL] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Derryl,
+            .pal = sFieldMugshotPal_Derryl,
+        },
+    },
+        [MUGSHOT_STELIO] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Stelio,
+            .pal = sFieldMugshotPal_Stelio,
+        },
+    },
+        [MUGSHOT_ADELINE] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Adeline,
+            .pal = sFieldMugshotPal_Adeline,
+        },
+    },
+        [MUGSHOT_LAIRA] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Laira,
+            .pal = sFieldMugshotPal_Laira,
+        },
+    },
+        [MUGSHOT_AUTUMN] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Autumn,
+            .pal = sFieldMugshotPal_Autumn,
+        },
+    },
+        [MUGSHOT_RABA] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Raba,
+            .pal = sFieldMugshotPal_Raba,
+        },
+    },
+        [MUGSHOT_ALTAIR] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Altair,
+            .pal = sFieldMugshotPal_Altair,
+        },
+    },
+        [MUGSHOT_ZACHARY] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Zachary,
+            .pal = sFieldMugshotPal_Zachary,
+        },
+    },
+        [MUGSHOT_JADE] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Jade,
+            .pal = sFieldMugshotPal_Jade,
+        },
+    },
+        [MUGSHOT_RED] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Red,
+            .pal = sFieldMugshotPal_Red,
+        },
+    },
+        [MUGSHOT_BLUE] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Blue,
+            .pal = sFieldMugshotPal_Blue,
+        },
+    },
+        [MUGSHOT_LANCE] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Lance,
+            .pal = sFieldMugshotPal_Lance,
+        },
+    },
+        [MUGSHOT_STEVEN] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Steven,
+            .pal = sFieldMugshotPal_Steven,
+        },
+    },
+        [MUGSHOT_CYNTHIA] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Cynthia,
+            .pal = sFieldMugshotPal_Cynthia,
+        },
+    },
+/*        [MUGSHOT_IRIS] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Iris,
+            .pal = sFieldMugshotPal_Iris,
+        },
+    },
+        [MUGSHOT_COLRESS] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Colress,
+            .pal = sFieldMugshotPal_Colress,
+        },
+    },
+    */
+       [MUGSHOT_DIANTHA] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Diantha,
+            .pal = sFieldMugshotPal_Diantha,
+        },
+    },
+        [MUGSHOT_LEON] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Leon,
+            .pal = sFieldMugshotPal_Leon,
+        },
+    },
+        [MUGSHOT_NEMONA] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Nemona,
+            .pal = sFieldMugshotPal_Nemona,
+        },
+    },
+};
